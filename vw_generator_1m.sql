459e8bba0dbf756446a37419de29942800a8b61b
Kshitij Satpute <ksatpute123@gmail.com>
2017-12-15 14:44:27 +0530

    Create vw_generator_1m.sql

---
+++ vw_generator_1m.sql
@@ -0,0 +1,9 @@
+CREATE 
+    ALGORITHM = UNDEFINED
+    SQL SECURITY DEFINER
+VIEW `vw_generator_1m` AS
+    SELECT 
+        ((`hi`.`n` << 16) | `lo`.`n`) AS `n`
+    FROM
+        (`vw_generator_64k` `lo`
+        JOIN `vw_generator_16` `hi`)
